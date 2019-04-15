afb6c218cf87e8d9c68c81fee3d9102df543e9e7
Kevin Wang <kevinwang0324@gmail.com>
2019-04-15 15:12:16 -0700

    initial commit

---
+++ Module1/FirstProgram.java
@@ -0,0 +1,21 @@
+// This is a comment. When you run this program, the computer automatically ignores lines with 2 slashes in a row
+
+/*
+    This is the format for a multiline comment
+    These can be as long as you'd like
+
+*/
+
+package Module1; // A package is used to group related classes. Think of it as a folder in your file system.
+
+public class FirstProgram { // This the format for a class in java. All executable code goes in the class
+
+    public static void main(String[] args) { // This is known as your main method/function. When you run you program, it runs your main method
+
+        System.out.println("Hello World"); // This is a statement. A statement is an instruction that causes the program to perform an action
+        /*
+            All statements end in a semicolon
+            System.out.println(); is a statement that outputs whatever is in between the Parenthesis
+         */
+    }
+}
